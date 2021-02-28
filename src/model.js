37a439c14aeae5058cf38a42b17eed21f7bab093
Sergey Kryukov <krus-83@mail.ru>
2021-02-28 21:47:41 +0700

    some updates

--- src/model.js
+++ src/model.js
@@ -1,20 +1,43 @@
 import image from './assets/image.png';
 
 export const model = [
-    {
-        type: 'title',
-        value: 'Конструктор сайтов JS',
-        options: {
-            tag: 'h2'
+	{
+		type: 'title',
+		value: 'Конструктор сайтов JS',
+		options: {
+			tag: 'h2',
+			styles: {
+				background: 'linear-gradient(to right, #699fc9, #18466b)',
+				color: '#fff',
+				padding: '1.5rem',
+				'text-align': 'center'
+			}
+		}
+	},
+	{type: 'text', value: 'Here we go with some text'},
+	{
+		type: 'columns',
+      value: [
+          'Приложение на чистом JavaScript',
+          'Принципы SOLID и ООП в JavaScript в одном курсе',
+          'JavaScript - это очень интересно'
+      ],
+      options: {
+		    styles: {
+		        padding: '2rem',
+            'font-weight': 'bold'
         }
-    },
-    {type: 'text', value: 'Here we go with some text'},
-    {
-        type: 'columns', value: [
-            '111',
-            '222',
-            '333'
-        ]
-    },
-    {type: 'image', value: image},
+      }
+	},
+	{
+		type: 'image',
+		value: image,
+		options: {
+			styles: {
+				display: 'flex',
+				'justify-content': 'center',
+				padding: '2rem 0'
+			}
+		}
+	},
 ];
