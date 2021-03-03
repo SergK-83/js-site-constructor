fb861334e794cbc17c82e7baf435423799520fce
Sergey Kryukov <krus-83@mail.ru>
2021-03-03 21:05:23 +0700

    refatoring with classes

--- src/model.js
+++ src/model.js
@@ -1,56 +1,41 @@
+import {TitleBlock, TextBlock, ColumnsBlock, ImageBlock} from "./classes/blocks";
 import image from './assets/image.png';
 
 export const model = [
-	{
-		type: 'title',
-		value: 'Конструктор сайтов JS',
-		options: {
-			tag: 'h2',
-			styles: {
-				background: 'linear-gradient(to right, #699fc9, #18466b)',
-				color: '#fff',
-				padding: '1.5rem',
-				'text-align': 'center'
-			}
+	new TitleBlock('Конструктор сайтов JS', {
+		tag: 'h2',
+		styles: {
+			background: 'linear-gradient(to right, #699fc9, #18466b)',
+			color: '#fff',
+			padding: '1.5rem',
+			'text-align': 'center'
 		}
-	},
-	{
-		type: 'text',
-		value: 'Here we go with some text',
-		options: {
-			styles: {
-				padding: '1rem',
-				color: '#777'
-			}
+	}),
+	new TextBlock('Here we go with some text', {
+		styles: {
+			padding: '1rem',
+			color: '#777'
 		}
-	},
-	{
-		type: 'columns',
-		value: [
-			'Приложение на чистом JavaScript',
-			'Принципы SOLID и ООП в JavaScript в одном курсе',
-			'JavaScript - это очень интересно'
-		],
-		options: {
-			styles: {
-				padding: '1rem',
-				'font-weight': 'bold',
-			}
+	}),
+	new ColumnsBlock([
+		'Приложение на чистом JavaScript',
+		'Принципы SOLID и ООП в JavaScript в одном курсе',
+		'JavaScript - это очень интересно'
+	], {
+		styles: {
+			padding: '1rem',
+			'font-weight': 'bold',
 		}
-	},
-	{
-		type: 'image',
-		value: image,
-		options: {
-			styles: {
-				display: 'flex',
-				'justify-content': 'center',
-				padding: '1rem'
-			},
-			stylesImg: {
-				border: '2px solid #ccc'
-			},
-			alt: 'Картинка'
-		}
-	},
+	}),
+	new ImageBlock(image, {
+		styles: {
+			display: 'flex',
+			'justify-content': 'center',
+			padding: '1rem'
+		},
+		stylesImg: {
+			border: '2px solid #ccc'
+		},
+		alt: 'Картинка'
+	})
 ];
